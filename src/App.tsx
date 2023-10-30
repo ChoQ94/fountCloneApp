5211cb6a079f4650b232caaf59ad53438192a4d9
ChoQ94 <whrbdnjs1994@gmail.com>
2023-10-30 16:28:12 +0900

    feat: AppState 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React, {useRef, useState} from 'react';
-import {AppState, AppStateStatus, SafeAreaView} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {AppState, AppStateStatus, SafeAreaView, Text, View} from 'react-native';
 
 const App = () => {
   // 현재 앱 상태 확인 로직
   const appState = useRef(AppState.currentState);
   const [appStateVisible, setAppStateVisible] = useState(appState.current);
-  const stateUpdater = (nextAppState: AppStateStatus) => {
+  const appStateUpdater = (nextAppState: AppStateStatus) => {
     if (
       appState.current.match(/inactive|background/) &&
       nextAppState === 'active'
     ) {
       /// Foreground 상태로 돌아온 경우 Health Check 구동
-      console.log('come back');
     }
+    console.log(nextAppState);
     appState.current = nextAppState;
     setAppStateVisible(appState.current);
   };
-  console.log(typeof appState.current);
 
-  return <SafeAreaView />;
+  useEffect(() => {
+    AppState.addEventListener('change', appStateUpdater);
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>g222333</Text>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default App;
